68d0995a53a57ea009917cddc874a647c1e4ed77
Jongmin Jack Kim <jmjkim00@icloud.com>
2022-01-27 19:35:24 -0800

    Completed optional assignment

--- javascript/reverse_string.js
+++ javascript/reverse_string.js
@@ -1,6 +1,17 @@
 function reverseString(str) {
   // type your code here
+
+  let stringCount = str.length;
+  let reversed = "";
+
+  do {
+      stringCount -= 1;
+      reversed += str[stringCount];
+  } while (stringCount > 0);
+
+  console.log(reversed);
 }
+reverseString("catbaby");
 
 if (require.main === module) {
   // add your own tests in here
@@ -16,4 +27,27 @@ if (require.main === module) {
 module.exports = reverseString;
 
 // Please add your pseudocode to this file
+
+/* pseudocode
+::To help understand this untidy pseudocode.. think of the string count number is a label (NOT INDEX!) for each letter of your 'target'.
+Exmaple: "hello" has string count of 5, label of the each letter is as:
+h = 1
+e = 2
+l = 3
+l = 4
+o = 5
+
+SET: 2 variables, one that has the string count of your 'target' (where iteration begins) and another one for holding the iterated value.
+FOR: iterating each string of your 'target' => accessing to each letter.
+IF: string count is correct, then do (current string count - 1) while string count is bigger than 0 => this will iterate your 'target' from 'right to left'.
+THEN: store the iterated value (each letter) to the variable name 'reversed' (line 5).
+DECREMENT: string count 
+END IF: string count is 0
+*/
+
 // And a written explanation of your solution
+
+/* 
+The variable on line 9 tells the while loop to iterate over the index of the argument backwardly and then 
+appends the each iterated value (cycles back to its variable).
+*/
